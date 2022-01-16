87b08e5fbd9d2a1c2cc335272f202161ffd80244
Nikhil-2398 <97340848+Nikhil-2398@users.noreply.github.com>
2022-01-16 13:43:13 +0530

    Update Assignment 1.c

--- Assignment 1/Assignment 1.c
+++ Assignment 1/Assignment 1.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 
 int main()
-{unsigned char Out = 0x01;
-unsigned char G; 
+{
+    unsigned char Out = 0x01;
+    unsigned char G; 
     unsigned char X[8] = { 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01};
     unsigned char Y[8] = { 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x01, 0x01};
     unsigned char Z[8] = { 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01};
-    unsigned char a, b, expression_ans;
+    
 
     for (int i = 0; i < 8; i++) {
-
+        // Using Nand Logic
+        int p=!(X[i]&&Z[i]);
+        int q= !(X[i]&&(!(Y[i])));
+        int r=   !((!(X[i]))&&(Y[i]&&(!(Z[i]))));
+        G=!(p&&q&&r);
     
-            G=!((!(X[i]&&Y[i]))&&(!(X[i]&&Z[i]))&&(!(Y[i]&&(!(Z[i])))));
+            
              printf("%x%x%x",Out&X[i],Out&Y[i],Out&Z[i]);
     printf(" ");
     printf("%x\n" ,Out&G);
@@ -27,9 +32,9 @@ unsigned char G;
 001 0
 010 1
 011 0
-100 0
+100 1
 101 1
-110 1
+110 0
 111 1
 
 */
