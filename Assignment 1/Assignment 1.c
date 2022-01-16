3ffd488c3adc0737d0be4631db7292bd33b8451b
Nikhil-2398 <97340848+Nikhil-2398@users.noreply.github.com>
2022-01-16 15:48:04 +0530

    Update Assignment 1.c

--- Assignment 1/Assignment 1.c
+++ Assignment 1/Assignment 1.c
@@ -3,24 +3,25 @@
 
 int main()
 {
-    unsigned char Out = 0x01;
+    
     unsigned char G; 
     unsigned char X[8] = { 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01};
     unsigned char Y[8] = { 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x01, 0x01};
     unsigned char Z[8] = { 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01};
+    unsigned char Out = 0x01;
     
 
     for (int i = 0; i < 8; i++) {
         // Using Nand Logic
-        int p=!(X[i]&&Z[i]);
+        int p= !(X[i]&&Z[i]);
         int q= !(X[i]&&(!(Y[i])));
-        int r=   !((!(X[i]))&&(Y[i]&&(!(Z[i]))));
+        int r= !((!(X[i]))&&(Y[i]&&(!(Z[i]))));
         G=!(p&&q&&r);
     
             
              printf("%x%x%x",Out&X[i],Out&Y[i],Out&Z[i]);
-    printf(" ");
-    printf("%x\n" ,Out&G);
+             printf(" ");
+             printf("%x\n" ,Out&G);
    
     }
     return 0;
